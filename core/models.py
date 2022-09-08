4e80209d87dd5d5743a3d65a61a02fb2d027194b
Guilherme Bragato Albanaz <guibragatoalbnz@gmail.com>
2022-09-08 20:47:40 -0300

    models pt1

--- core/models.py
+++ core/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+class Jogos(models.Model):
+    def __str__(self):
+        return self.nome
+    data = models.DateField()
+    idade = models.IntegerField()
+    nome = models.CharField(max_length=200)
+
+class Usuario(models.Model):
+    def __str__(self):
+        return self.nome
+    senha = models.CharField(max_length=45)
+    data = models.DateField(verbose_name='data de nascimento')
+    email = models.CharField(max_length=120)
+    fone = models.CharField(max_length=12)
+    typeuser = models.BooleanField(default=False)
+    nome = models.CharField(max_length=50)
+
+
+class Resenha(models.Model):
+    def __str__(self):
+        return self.titulo
+    titulo = models.CharField(max_length=100)    
+    descricao = models.CharField(max_length=5000, verbose_name='descrição da resenha')
+    data = models.DateTimeField(verbose_name='data da publicação')
+    links = models.CharField(max_length=500)
+    jogo = models.ForeignKey(Jogos, on_delete=models.PROTECT)
+    usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
+
+class Curtidas(models.Model):
+    def __str__(self):
+        return (self.usuario + ' curtiu ' + self.resenha)
+    usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
+    resenha = models.ForeignKey(Resenha, on_delete=models.PROTECT)
+    data = models.DateTimeField()
+
+class Comentario(models.Model):
+    def __str__(self):
+        return (self.usuario + ' comentou em ' + self.resenha)
+    comentario = models.CharField(max_length=150)
+    usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
+    resenha = models.ForeignKey(Resenha, on_delete=models.PROTECT)
+    data = models.DateTimeField()
+
+
+
+    
