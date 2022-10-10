e96fd6c0bddc7379d313cd62769b9082713a6942
Guilherme Bragato Albanaz <guibragatoalbnz@gmail.com>
2022-10-10 16:51:32 -0300

    continuação django

--- askon/settings.py
+++ askon/settings.py
@@ -37,18 +37,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     "rest_framework",
+    "rest_framework_simplejwt",
     "core",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'askon.urls'
@@ -123,3 +126,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissions",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+# from rest_framework.permissions import IsAuthenticated
 
 from core.models import Resenha, Usuario, Comentario, Generos, Jogos, Curtidas
 from core.serializers import ResenhaSerializer, UsuarioSerializer, ComentarioSerializer, GenerosSerializer, CurtidasSerializer, JogosSerializer 
@@ -6,23 +7,29 @@ from core.serializers import ResenhaSerializer, UsuarioSerializer, ComentarioSer
 class ResenhaViewSet(ModelViewSet):
     queryset = Resenha.objects.all()
     serializer_class = ResenhaSerializer
+    # permission_classes = [IsAuthenticated]
+    
 
 class UsuarioViewSet(ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
+    # permission_classes = [IsAuthenticated]
 
 class ComentarioViewSet(ModelViewSet):
     queryset = Comentario.objects.all()
-    serializer_class = ComentarioSerializer
+    # serializer_class = ComentarioSerializer
 
 class GenerosViewSet(ModelViewSet):
     queryset = Generos.objects.all()
     serializer_class = GenerosSerializer
+    # permission_classes = [IsAuthenticated]
 
 class CurtidasViewSet(ModelViewSet):
     queryset = Curtidas.objects.all()
     serializer_class = CurtidasSerializer
+    # permission_classes = [IsAuthenticated]
 
 class JogosViewSet(ModelViewSet):
     queryset = Jogos.objects.all()
-    serializer_class = JogosSerializer    
+    serializer_class = JogosSerializer
+    # permission_classes = [IsAuthenticated]    
