5d7d3c7d45407a03b2d05a02ca310a678d9343d9
Guilherme Bragato Albanaz <guibragatoalbnz@gmail.com>
2022-10-16 22:51:46 -0300

    hash password usuario serializer

--- core/serializers/usuarios.py
+++ core/serializers/usuarios.py
@@ -7,3 +7,11 @@ class UsuarioSerializer(ModelSerializer):
     class Meta:
         model = Usuario
         fields = '__all__'
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
