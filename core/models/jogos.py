4b973501806a7a6853784cfe890d3af4c5605318
ericopieri <erico.pieri@gmail.com>
2022-11-01 14:47:15 -0300

    preparação para o aws (campo imagem em jogos)

--- core/models/jogos.py
+++ core/models/jogos.py
@@ -2,9 +2,11 @@ from django.db import models
 from core.models import Generos
 
 class Jogos(models.Model):
-    def __str__(self):
-        return self.nome
+    imagem = models.ImageField(upload_to="askon/jogos/")
     data = models.DateField()
     idade = models.IntegerField()
     nome = models.CharField(max_length=200)
     genero = models.ManyToManyField(Generos, related_name='generos')
+
+    def __str__(self):
+        return self.nome
