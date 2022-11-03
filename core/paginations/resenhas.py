09c23dcd0b9c69359eb80fd20bcc946998917c31
ericopieri <erico.pieri@gmail.com>
2022-11-03 17:27:38 -0300

    page_size 4

--- core/paginations/resenhas.py
+++ core/paginations/resenhas.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class ResenhaPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 4
     page_size_query_param = "page_size"
     max_page_size = 100
