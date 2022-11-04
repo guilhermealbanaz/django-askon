3af9507d3c22f863c28a9202ea139c99daf89375
ericopieri <erico.pieri@gmail.com>
2022-11-04 10:39:05 -0300

    update register

--- core/views/usuarios.py
+++ core/views/usuarios.py
@@ -1,14 +1,19 @@
 from rest_framework.viewsets import ModelViewSet
-from core.serializers import UsuarioSerializer
+from core.serializers import UsuarioSerializer, UsuarioPostSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from core.models import Usuario
 
 
 class UsuarioViewSet(ModelViewSet):
     queryset = Usuario.objects.all()
-    serializer_class = UsuarioSerializer
 
     def get_permissions(self):
         if self.action == "create":
             return [AllowAny()]
         return [IsAuthenticated()]
+    
+    def get_serializer_class(self):
+        if self.action == "create":
+            return UsuarioPostSerializer
+        
+        return UsuarioSerializer
